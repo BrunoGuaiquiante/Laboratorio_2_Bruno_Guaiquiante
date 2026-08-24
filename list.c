7098246c17f6e6da3ac847e638ef02fd3080d100
Your Name <you@example.com>
2026-08-24 01:24:27 +0000

    new try

--- list.c
+++ list.c
@@ -39,7 +39,7 @@ List * createList() {
     if(L!=NULL){
         L->head=NULL;
         L->tail=NULL;
-        L->current=NULL
+        L->current=NULL;
     }
      return L;
 }
@@ -49,7 +49,7 @@ List * createList() {
 //   - La segunda función retorna el dato del nodo a continuación del current y actualiza el current para que apunte a ese nodo.
 
 void * firstList(List * list) {
-    if(list==NULL || list->head=NULL){
+    if(list==NULL || list->head==NULL){
         return NULL;
     }
     list->current=list->head;
